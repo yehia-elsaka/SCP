d4bbb428fa8c2bb2fce1bc0aebec859b468e47a2
yehia-elsaka <yahya.elsaka@gmail.com>
2015-10-04 11:48:08 +0200

    services controller

---
+++ SCP/ServicesController.h
@@ -0,0 +1,14 @@
+//
+//  ServicesController.h
+//  SCP
+//
+//  Created by mac on 10/4/15.
+//  Copyright (c) 2015 TrianglZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ServicesController : UIViewController
+- (IBAction)back:(id)sender;
+
+@end
---
+++ SCP/ServicesController.m
@@ -0,0 +1,41 @@
+//
+//  ServicesController.m
+//  SCP
+//
+//  Created by mac on 10/4/15.
+//  Copyright (c) 2015 TrianglZ. All rights reserved.
+//
+
+#import "ServicesController.h"
+
+@interface ServicesController ()
+
+@end
+
+@implementation ServicesController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)back:(id)sender {
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+@end
