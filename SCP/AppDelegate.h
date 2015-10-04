99f33a21508f13c692e4221fb65a2adb9b3c1ec0
yehia-elsaka <yahya.elsaka@gmail.com>
2015-10-04 10:31:37 +0200

    first commit

---
+++ SCP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCP
+//
+//  Created by mac on 10/3/15.
+//  Copyright (c) 2015 TrianglZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SCP/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  SCP
+//
+//  Created by mac on 10/3/15.
+//  Copyright (c) 2015 TrianglZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
